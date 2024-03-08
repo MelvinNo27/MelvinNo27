0c3d34dd44621a97d72a561f2497d65113b4ea82
Melvin Magsalay <melvinmagsalay27@gmail.com>
2024-03-08 19:03:54 +0800

    Activities

--- Activity7-Palindrome.kt
+++ Activity7-Palindrome.kt
@@ -1,21 +1,29 @@
-fun main()
-{
-    print("Enter a string: ")
-    val enter = readln() ?:""
+fun main() {
+    while (true) {
+        println("Enter a string:")
+        val inputString = readLine() ?: ""
 
-    if (isPalindrome(enter.replace("","")))
-    {
-        print("$enter is Palindrome")
-    }else{
-        print("$enter is not Palindrome")
+        if (isPalindrome(inputString.replace(" ", ""))) {
+            println("$inputString is Palindrome")
+        } else {
+            println("$inputString is Not Palindrome")
+        }
+        print("do you want to continue? [Yes/No]: ")
+        val ans = readLine()?.toLowerCase()
+        if(ans == "yes")
+        {
+            continue
+        }else{
+            println("Thanks have a good day!")
+            break
+        }
     }
 }
-fun isPalindrome(str: String): Boolean{
+
+fun isPalindrome(str: String): Boolean {
     val length = str.length
-    for (i in 0 until length / 2)
-    {
-        if(str[i] != str[length - i - 1])
-        {
+    for (i in 0 until length / 2) {
+        if (str[i] != str[length - i - 1]) {
             return false
         }
     }
